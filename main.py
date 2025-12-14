8667551bb6774fa296973ab636081cb1d733e37f
SyrineOunais <syrine.ounais@esprit.tn>
2025-12-14 13:05:10 +0100

    Docker

--- main.py
+++ main.py
@@ -12,31 +12,13 @@ from model_pipeline import (
     save_model, train_model
 )
 
-DB_PATH = os.getenv("DB_PATH", "mlflow.db")  # mlflow.db pour local, variable pour CI
 
-def init_db(path):
-    """Crée la DB et les tables si elles n'existent pas."""
-    conn = sqlite3.connect(path)
-    cursor = conn.cursor()
-
-    # Exemple de table pour MLflow (ajuster si besoin)
-    cursor.execute("""
-        CREATE TABLE IF NOT EXISTS experiments (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT NOT NULL,
-            created_at TEXT
-        )
-    """)
-
-    conn.commit()
-    conn.close()
 
 # Initialisation de la DB avant tout
 init_db(DB_PATH)
 
 def main():
-    mlflow.set_tracking_uri("sqlite:///{DB_PATH}") #pour le push git 
-    #mlflow.set_tracking_uri("sqlite:///mlflow.db")
+    mlflow.set_tracking_uri("sqlite:///mlflow.db")
    # Définir l'expérience MLflowss
     mlflow.set_experiment("Loan_Prediction_RandomForest")
     #mlflow.set_default_artifact_uri("file:./artifacts")
